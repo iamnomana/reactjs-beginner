2bb4d163b840b29a2aaa2bf3d005048e0ea1a431
Divine Azianka <aziankad@gmail.com>
2022-03-22 15:08:38 +0000

    Update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>I am Divine Nomana Azianka. A Full-Stack Software Engineer.👨🏽‍💻😉</h1>
+    </>
+  );
+}
+
+export default App;
